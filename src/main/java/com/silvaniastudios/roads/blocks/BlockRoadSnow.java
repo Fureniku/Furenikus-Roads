b85240eb0e4124f3346b3fbb964d09b3c5a480e3
Fureniku <fureniku@gmail.com>
2020-11-20 14:31:26 +0000

    Fixed #34

--- src/main/java/com/silvaniastudios/roads/blocks/BlockRoadSnow.java
+++ src/main/java/com/silvaniastudios/roads/blocks/BlockRoadSnow.java
@@ -1,8 +1,12 @@
 package com.silvaniastudios.roads.blocks;
 
+import javax.annotation.Nullable;
+
 import com.google.common.collect.ImmutableList;
 import com.silvaniastudios.roads.FurenikusRoads;
+import com.silvaniastudios.roads.blocks.paint.PaintBlockBase;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockSnow;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
@@ -12,6 +16,7 @@ import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
@@ -62,6 +67,29 @@ public class BlockRoadSnow extends BlockSnow {
         	return new Vec3d(0, -1 + 0.0625 + (meta*0.0625), 0);
         }
         return super.getOffset(state, worldIn, pos);
+    }
+	
+	@Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    	return new AxisAlignedBB(0.0D, -1+getBlockBelowHeight(source, pos), 0.0D, 1.0D, -1+getBlockBelowHeight(source, pos)+0.0625, 1.0D);
+    }
+
+    @Nullable
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return NULL_AABB;
+    }
+    
+    @SuppressWarnings("deprecation")
+	public double getBlockBelowHeight(IBlockAccess worldIn, BlockPos pos) {
+    	IBlockState underState = worldIn.getBlockState(pos.offset(EnumFacing.DOWN));
+        Block underBlock = underState.getBlock();
+        double extraOffset = 0.0;
+        
+        if (underBlock instanceof PaintBlockBase || underBlock instanceof NonPaintRoadTopBlock || underBlock instanceof CurbBlock) {
+        	extraOffset = 0.062;
+        }
+        
+        return underBlock.getBoundingBox(underState, worldIn, pos.offset(EnumFacing.DOWN)).maxY - extraOffset;
     }
 
 }
