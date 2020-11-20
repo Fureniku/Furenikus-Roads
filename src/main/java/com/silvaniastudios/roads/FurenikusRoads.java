ff130be037323bb5ca76e767d8b2fbd8a6d18b47
Fureniku <fureniku@gmail.com>
2020-11-20 14:39:25 +0000

    Fixed grass full road not taking grass colour, version up: 1.1.9

--- src/main/java/com/silvaniastudios/roads/FurenikusRoads.java
+++ src/main/java/com/silvaniastudios/roads/FurenikusRoads.java
@@ -38,7 +38,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class FurenikusRoads {
 	
 	public static final String MODID = "furenikusroads";
-	public static final String VERSION = "1.1.8";
+	public static final String VERSION = "1.1.9";
 	
 	@Instance(MODID)
 	public static FurenikusRoads instance;
