2973390d254cb031b5d75a01c380a7e8976f1969
Fureniku <fureniku@gmail.com>
2023-11-15 18:36:06 +0000

    1.2.6

--- src/main/java/com/silvaniastudios/roads/FurenikusRoads.java
+++ src/main/java/com/silvaniastudios/roads/FurenikusRoads.java
@@ -60,7 +60,7 @@ import java.util.zip.ZipInputStream;
 public class FurenikusRoads {
 	
 	public static final String MODID = "furenikusroads";
-	public static final String VERSION = "1.2.5";
+	public static final String VERSION = "1.2.6";
 	
 	@Instance(MODID)
 	public static FurenikusRoads instance;
